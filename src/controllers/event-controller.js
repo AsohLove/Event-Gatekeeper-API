bdfdc8885762638ee5ac028a1344cebca0a314ab
Love Asoh <loveasoh@gmail.com>
2026-07-07 16:01:51 +0100

    feat: implement event controllers and models for routes handlers and running SQL

---
+++ src/controllers/event-controller.js
@@ -0,0 +1,51 @@
+import * as events from '../models/event-model.js'
+import createError from 'http-errors'
+
+export async function createNewEvent(req, res, next) {
+    try {
+        const created = await events.createEvent(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: created
+        });
+    } catch (err) {
+        next(err)
+    }
+}
+
+export async function getSingleEvent(req, res, next){
+    try {
+        
+        const event = await events.findEventById(req.params.id);
+
+        if (!event) {
+            throw createError(404, "Event is not found")
+        }
+
+        res.json({
+            success: true,
+            data: event
+        })
+
+    } catch (err) {
+            next(err)
+    }
+}
+
+export async function getEvents(req, res, next){
+    try {
+        const {after, limit } = req.query;
+
+        const eventRows = await events.findEvents(after, limit);
+
+        res.json({
+            success: true,
+            data: eventRows,
+            nextCursor:
+                eventRows.length ? eventRows.at(-1).id : null
+        });
+    } catch (err) {
+        next(err)
+    }
+}
---
+++ src/models/event-model.js
@@ -0,0 +1,40 @@
+import { pool } from '../db/dbConnect.js'
+
+export async function createEvent({name, venue, starts_at, capacity}){
+    const { rows } = await pool.query(`
+            INSERT INTO events 
+                (name, venue, starts_at, capacity, seats_remaining)
+            VALUES
+                ($1, $2. $3, $4, $4)
+            RETURNING *;
+
+        `, [name, venue, starts_at, capacity]
+    );
+
+    return rows[0];
+}
+
+export async function findEventById(id){
+    const { rows } = await pool.query(`
+            SELECT * 
+            FROM events
+            WHERE id = $1;
+        `, [id]
+    );
+
+    return rows[0];
+}
+
+export async function findEvents(after = 0, limit = 10) {
+    const { rows } = await pool.query(`
+            SELECT * 
+            FROM events
+            WHERE id > $1
+            ORDER BY id
+            LIMIT $2;
+
+        `, [after, limit]
+    );
+
+    return rows[0];
+}
